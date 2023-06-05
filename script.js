16de2c6a7c7153cc12588ce6b66ff543408a6775
JeevithaSimma <93779394+JeevithaSimma@users.noreply.github.com>
2023-06-05 21:40:45 +0530

    performed DOM manipulation

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hi I,m Jeevitha");
+var data1={image : "bz2.jpg",name : "Jeevitha Simma",designation : "Student"}
+var data2={image : "xyz.jpg",name : "Jeevi",designation : "Software Developer"}
+var isdata1 = true;
+var displaydata;
+var changemypicture = function(){
+    if(isdata1){
+        displaydata = data2;
+        isdata1 = false;
+    }
+    else{
+        displaydata = data;
+        isdata1 = true;
+    }
+    document.getElementById("mypicture").src=displaydata.image;
+}
